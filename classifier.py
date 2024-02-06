1bd8da16b4b1ca8b5decc34cf9255b20f471db77
Tirth Patel <t83patel@uwaterloo.ca>
2024-02-05 22:56:57 -0500

    classifier edits

--- classifier.py
+++ classifier.py
@@ -18,3 +18,27 @@ Symptom_2 = le.fit_transform(list(dataset["Symptom_2"]))
 Symptom_3 = le.fit_transform(list(dataset["Symptom_3"]))
 
 Symptom_4 = le.fit_transform(list(dataset["Symptom_4"]))
+
+everitylist = le.fit_transform(list(severity["Symptom"]))
+def take_input():
+    symptom_1 = input("What symptom are you feeling?: ").lower().replace(" ", "_")
+    symptom_2 = input("What symptom are you feeling?: ").lower().replace(" ", "_")
+    symptom_3 = input("What symptom are you feeling?: ").lower().replace(" ", "_")
+    symptom_4 = input("What symptom are you feeling?: ").lower().replace(" ", "_")
+    symptoms = symptom_1 + " " + symptom_2 + " " + symptom_3 + " " + symptom_4
+    symptoms_list = symptoms.split()
+    symptoms_list = le.transform(symptoms_list)
+    symptoms_list = np.array(list(zip(symptoms_list)))
+    return symptoms_list, type(symptoms_list), Symptom_1
+
+X = np.array(list(zip(Symptom_1, Symptom_2, Symptom_3, Symptom_4)))
+y = np.array(dataset[predict])
+
+#X = np.array(dataset.drop([predict], 1))
+#y = np.array(dataset[predict])
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2)
+model = RandomForestClassifier(n_estimators=25)
+model.fit(x_train, y_train)
+acc = model.score(x_test, y_test)
+print("The Accuracy of this prediction is", acc)
+pred = model.predict(x_test)
